296ab8993d273c43af86a53d48ad22485729f9dd
Melon <guang.xing@liulishuo.com>
2019-07-10 20:31:43 +0800

    feat: change result input type

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app" :class="showPCStyle ? 'is-pc' : ''">
     <h3>Live password</h3>
-    <mt-field label="记忆密码" placeholder="与个人信息无关的密码" type="password" v-model="memoryKey"></mt-field>
-    <mt-field label="区分代号" placeholder="taobao、weixin、weibo ..." v-model="specialKey"></mt-field>
-    <mt-field label="密码位数" placeholder="10~40" v-model="passwdNum"></mt-field>
+    <mt-field label="记忆密码" placeholder="与个人信息无关的密码" type="password" v-model="memoryKey" />
+    <mt-field label="区分代号" placeholder="taobao、weixin、weibo ..." v-model="specialKey" />
+    <mt-field label="密码位数" placeholder="10~40" v-model="passwdNum" />
     <mt-field label="包含大小写">
-      <mt-switch v-model="isContainUpper"></mt-switch>
+      <mt-switch v-model="isContainUpper" />
     </mt-field>
     <mt-field label="包含特殊字符">
-      <mt-switch v-model="isContainSpecial"></mt-switch>
+      <mt-switch v-model="isContainSpecial" />
     </mt-field>
-    <mt-field label="密码">
+    <mt-cell title="密码">
       <span>{{ result }}</span>
-    </mt-field>
-    <mt-button @click="handleCopy" type="primary" :disabled="!result">
-      点击复制密码
-    </mt-button>
+    </mt-cell>
+    <mt-button @click="handleCopy" type="primary" :disabled="!result">点击复制密码</mt-button>
   </div>
 </template>
 
 <script>
 import sha from 'js-sha256';
-import { Field, Switch, Button, Toast } from 'mint-ui';
+import { Field, Switch, Button, Toast, Cell } from 'mint-ui';
 import copy from 'copy-to-clipboard';
 
 const START_NUM = 5;
@@ -40,6 +38,7 @@ export default {
   components: {
     [Field.name]: Field,
     [Switch.name]: Switch,
+    [Cell.name]: Cell,
     [Button.name]: Button
   },
   methods: {
@@ -61,10 +60,10 @@ export default {
     }) {
       if (!memoryKey || !specialKey) return '';
       let shaResult = sha(sha(memoryKey) + sha(specialKey));
-      passwdNum = parseInt(passwdNum, 10)
-      if (passwdNum < 10) passwdNum = 10
-      if (passwdNum > 40) passwdNum = 40
-      if (!passwdNum || isNaN(passwdNum)) passwdNum = 16
+      passwdNum = parseInt(passwdNum, 10);
+      if (passwdNum < 10) passwdNum = 10;
+      if (passwdNum > 40) passwdNum = 40;
+      if (!passwdNum || isNaN(passwdNum)) passwdNum = 16;
       shaResult = shaResult.slice(START_NUM, +passwdNum + START_NUM);
       if (isContainUpper) {
         let i = 0;
@@ -88,7 +87,7 @@ export default {
     }
   },
   mounted() {
-    this.showPCStyle = window.innerWidth > 450
+    this.showPCStyle = window.innerWidth > 450;
   }
 };
 </script>
